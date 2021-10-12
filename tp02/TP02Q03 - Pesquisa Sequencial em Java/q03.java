b936fe46c9b53cdfb35aad9d07ca951f11b17a46
VIctor <colen.victor@icloud.com>
2021-10-12 15:36:40 -0300

    added q03

---
+++ tp02/TP02Q03 - Pesquisa Sequencial em Java/q03.java
@@ -0,0 +1,56 @@
+import java.io.RandomAccessFile;
+import java.time.Instant;
+import java.util.*;
+
+public class q03 {
+    public static void main(String[] args) {
+        long programStartTime = Instant.now().toEpochMilli();
+        Scanner scanner = new Scanner(System.in);
+        int comparisons = 0;
+
+        List<String> list = new ArrayList<String>();
+
+        while (true) {
+            String line = scanner.nextLine();
+
+            if (line.equals("FIM"))
+                break;
+            else
+                comparisons++;
+
+            list.add(line.replaceAll("(.html)", "").replaceAll("_", " "));
+        }
+
+        while (true) {
+            String line = scanner.nextLine();
+
+            comparisons++;
+            if (line.equals("FIM"))
+                break;
+
+            boolean isEqualToLine = false;
+
+            for (String seriesName : list) {
+                if (seriesName.equals(line)) {
+                    comparisons++;
+                    isEqualToLine = true;
+                    break;
+                } else {
+                    comparisons++;
+                }
+            }
+            System.out.println(isEqualToLine ? "SIM" : "NÃO");
+        }
+
+        long totalRuntime = Instant.now().toEpochMilli() - programStartTime;
+
+        try {
+            RandomAccessFile log = new RandomAccessFile("matricula_sequencial.txt", "rw");
+            log.writeChars(String.format("729414\t%d\t%d", totalRuntime / 1000, comparisons));
+            log.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
