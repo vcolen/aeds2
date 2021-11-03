3a0382e8c0f6ca57009264237b11314b76fac6b4
VIctor <colen.victor@icloud.com>
2021-11-03 18:34:24 -0300

    add q11

---
+++ tp03/TP03Q11 - Lista Flexível em Java/q11.java
@@ -0,0 +1,512 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+
+class Serie {
+
+    private String name;
+    private String format;
+    private String length;
+    private String country;
+    private String language;
+    private String broadcaster;
+    private String originalStream;
+    private String numberOfSeasons;
+    private String numberOfEpisodes;
+
+    public Serie() {
+        name = "";
+        format = "";
+        length = "";
+        country = "";
+        language = "";
+        broadcaster = "";
+        originalStream = "";
+        numberOfSeasons = "";
+        numberOfEpisodes = "";
+    }
+
+    public Serie(String name, String format, String length, String country, String language, String broadcaster,
+            String originalStream, String numberOfSeasons, String numberOfEpisodes) {
+
+        this.name = name;
+        this.format = format;
+        this.length = length;
+        this.country = country;
+        this.language = language;
+        this.broadcaster = broadcaster;
+        this.originalStream = originalStream;
+        this.numberOfSeasons = numberOfSeasons;
+        this.numberOfEpisodes = numberOfEpisodes;
+    }
+
+    public void printClass() {
+        System.out.println(this.name + " " + this.format + " " + this.length + " " + this.country + " " + this.language
+                + " " + this.broadcaster + " " + this.originalStream + " " + this.numberOfSeasons + " "
+                + this.numberOfEpisodes);
+    }
+
+    public Serie clone() {
+        Serie destination = new Serie();
+        destination.name = this.name;
+        destination.format = this.format;
+        destination.length = this.length;
+        destination.country = this.country;
+        destination.language = this.language;
+        destination.broadcaster = this.broadcaster;
+        destination.originalStream = this.originalStream;
+        destination.numberOfSeasons = this.numberOfSeasons;
+        destination.numberOfEpisodes = this.numberOfEpisodes;
+        return destination;
+    }
+
+    // MARK - Get properties
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getFormat() {
+        return this.format;
+    }
+
+    public String getDuration() {
+        return this.length;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public String getLanguage() {
+        return this.language;
+    }
+
+    public String getBroadcaster() {
+        return this.broadcaster;
+    }
+
+    public String getStreaming() {
+        return this.originalStream;
+    }
+
+    public String getSeasons() {
+        return this.numberOfSeasons;
+    }
+
+    public String getEpisodes() {
+        return this.numberOfEpisodes;
+    }
+
+    // MARK - Set methods
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public void setDuration(String length) {
+        this.length = length;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setBroadcaster(String broadcaster) {
+        this.broadcaster = broadcaster;
+    }
+
+    public void setStreaming(String originalStream) {
+        this.originalStream = originalStream;
+    }
+
+    public void setSeasons(String numberOfSeasons) {
+        this.numberOfSeasons = numberOfSeasons;
+    }
+
+    public void setEpisodes(String numberOfEpisodes) {
+        this.numberOfEpisodes = numberOfEpisodes;
+    }
+}
+
+class Celula {
+    public Serie elemento; // Elemento inserido na celula.
+    public Celula prox; // Aponta a celula prox.
+
+    public Celula() {
+
+    }
+
+    /**
+     * Construtor da classe.
+     * 
+     * @param elemento int inserido na celula.
+     */
+    public Celula(Serie elemento) {
+        this.elemento = elemento;
+        this.prox = null;
+    }
+}
+
+class Lista {
+    private Celula primeiro;
+    private Celula ultimo;
+
+    /**
+     * Construtor da classe que cria uma lista sem elementos (somente no cabeca).
+     */
+    public Lista() {
+        primeiro = new Celula();
+        ultimo = primeiro;
+    }
+
+    /**
+     * Insere um elemento na primeira posicao da lista.
+     * 
+     * @param x Serie elemento a ser inserido.
+     */
+    public void inserirInicio(Serie x) {
+        Celula tmp = new Celula(x);
+        tmp.prox = primeiro.prox;
+        primeiro.prox = tmp;
+        if (primeiro == ultimo) {
+            ultimo = tmp;
+        }
+        tmp = null;
+    }
+
+    /**
+     * Insere um elemento na ultima posicao da lista.
+     * 
+     * @param x Serie elemento a ser inserido.
+     */
+    public void inserirFim(Serie x) {
+        ultimo.prox = new Celula(x);
+        ultimo = ultimo.prox;
+    }
+
+    /**
+     * Remove um elemento da primeira posicao da lista.
+     * 
+     * @return resp Serie elemento a ser removido.
+     * @throws Exception Se a lista nao contiver elementos.
+     */
+    public Serie removerInicio() throws Exception {
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+        }
+
+        Celula tmp = primeiro;
+        primeiro = primeiro.prox;
+        Serie resp = primeiro.elemento;
+        tmp.prox = null;
+        tmp = null;
+        return resp;
+    }
+
+    /**
+     * Remove um elemento da ultima posicao da lista.
+     * 
+     * @return resp Serie elemento a ser removido.
+     * @throws Exception Se a lista nao contiver elementos.
+     */
+    public Serie removerFim() throws Exception {
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+        }
+
+        // Caminhar ate a penultima celula:
+        Celula i;
+        for (i = primeiro; i.prox != ultimo; i = i.prox)
+            ;
+
+        Serie resp = ultimo.elemento;
+        ultimo = i;
+        i = ultimo.prox = null;
+
+        return resp;
+    }
+
+    /**
+     * Insere um elemento em uma posicao especifica considerando que o primeiro
+     * elemento valido esta na posicao 0.
+     * 
+     * @param x   Serie elemento a ser inserido.
+     * @param pos int posicao da insercao.
+     * @throws Exception Se <code>posicao</code> invalida.
+     */
+    public void inserir(Serie x, int pos) throws Exception {
+
+        int tamanho = tamanho();
+
+        if (pos < 0 || pos > tamanho) {
+            throw new Exception("Erro ao inserir posicao (" + pos + " / tamanho = " + tamanho + ") invalida!");
+        } else if (pos == 0) {
+            inserirInicio(x);
+        } else if (pos == tamanho) {
+            inserirFim(x);
+        } else {
+            // Caminhar ate a posicao anterior a insercao
+            Celula i = primeiro;
+            for (int j = 0; j < pos; j++, i = i.prox)
+                ;
+
+            Celula tmp = new Celula(x);
+            tmp.prox = i.prox;
+            i.prox = tmp;
+            tmp = i = null;
+        }
+    }
+
+    /**
+     * Remove um elemento de uma posicao especifica da lista considerando que o
+     * primeiro elemento valido esta na posicao 0.
+     * 
+     * @param posicao Meio da remocao.
+     * @return resp int elemento a ser removido.
+     * @throws Exception Se <code>posicao</code> invalida.
+     */
+    public Serie remover(int pos) throws Exception {
+        Serie resp;
+        int tamanho = tamanho();
+
+        if (primeiro == ultimo) {
+            throw new Exception("Erro ao remover (vazia)!");
+
+        } else if (pos < 0 || pos >= tamanho) {
+            throw new Exception("Erro ao remover (posicao " + pos + " / " + tamanho + " invalida!");
+        } else if (pos == 0) {
+            resp = removerInicio();
+        } else if (pos == tamanho - 1) {
+            resp = removerFim();
+        } else {
+            // Caminhar ate a posicao anterior a insercao
+            Celula i = primeiro;
+            for (int j = 0; j < pos; j++, i = i.prox)
+                ;
+
+            Celula tmp = i.prox;
+            resp = tmp.elemento;
+            i.prox = tmp.prox;
+            tmp.prox = null;
+            i = tmp = null;
+        }
+
+        return resp;
+    }
+
+    /**
+     * Mostra os elementos da lista separados por espacos.
+     */
+    public void mostrar() {
+        for (Celula i = primeiro.prox; i != null; i = i.prox)
+            i.elemento.printClass();
+    }
+
+    /**
+     * Procura um elemento e retorna se ele existe.
+     * 
+     * @param x Elemento a pesquisar.
+     * @return <code>true</code> se o elemento existir, <code>false</code> em caso
+     *         contrario.
+     */
+    public boolean pesquisar(Serie x) {
+        boolean resp = false;
+        for (Celula i = primeiro.prox; i != null; i = i.prox) {
+            if (i.elemento == x) {
+                resp = true;
+                i = ultimo;
+            }
+        }
+        return resp;
+    }
+
+    /**
+     * Calcula e retorna o tamanho, em numero de elementos, da lista.
+     * 
+     * @return resp int tamanho
+     */
+    public int tamanho() {
+        int tamanho = 0;
+        for (Celula i = primeiro; i != ultimo; i = i.prox, tamanho++)
+            ;
+        return tamanho;
+    }
+}
+
+class Read {
+    public static String justInt(String line) {
+        String auxString = "";
+        for (int i = 0; i < line.length(); i++) {
+            // caso o caracter seja um número ele é concatenado a variável auxString
+            if (line.charAt(i) >= '0' && line.charAt(i) <= '9') {
+                auxString += line.charAt(i);
+            } else {
+                break;
+            }
+        }
+        return auxString;
+    }
+
+    public static String removeTags(String line) {
+        line = line.replaceAll("<.*?>", ""); // removing tags
+        line = line.replaceAll("&.*?;", ""); // removing weird stuff
+        line = line.trim();
+        line = line.replaceAll("(lista de episódios)", "");
+        line = line.replaceAll("(Lista de episódios)", "");
+        line = line.replaceAll("(até o momento)", "");
+
+        return line;
+    }
+
+    // retrieving the name of the series
+    public static String searchName(String fileName) {
+        return fileName.replaceAll("(.html)", "").replaceAll("_", " ");
+    }
+
+    public static Serie readClass(String fileName) throws IOException {
+
+        String name;
+        String format;
+        String length;
+        String country;
+        String language;
+        String broadcaster;
+        String originalStream;
+        String numberOfSeasons;
+        String numberOfEpisodes;
+
+        FileReader fileReader = new FileReader("/tmp/series/" + fileName);
+
+        BufferedReader br = new BufferedReader(fileReader);
+
+        name = searchName(fileName);
+
+        while (!br.readLine().contains("Formato"))
+            ;
+        format = removeTags(br.readLine());
+
+        while (!br.readLine().contains("Duração"))
+            ;
+        length = removeTags(br.readLine());
+
+        while (!br.readLine().contains("País de origem"))
+            ;
+        country = removeTags(br.readLine());
+
+        while (!br.readLine().contains("Idioma original"))
+            ;
+        language = removeTags(br.readLine());
+
+        while (!br.readLine().contains("Emissora de televisão"))
+            ;
+        broadcaster = removeTags(br.readLine());
+
+        while (!br.readLine().contains("Transmissão original"))
+            ;
+        originalStream = removeTags(br.readLine());
+
+        while (!br.readLine().contains("N.º de temporadas"))
+            ;
+        numberOfSeasons = justInt(removeTags(br.readLine()));
+
+        while (!br.readLine().contains("N.º de episódios"))
+            ;
+        numberOfEpisodes = justInt(removeTags(br.readLine()));
+
+        br.close();
+        return new Serie(name, format, length, country, language, broadcaster, originalStream, numberOfSeasons,
+                numberOfEpisodes);
+
+    }
+}
+
+public class q11 {
+    public static void main(String[] args) {
+        String line;
+        Lista series = new Lista();
+        Scanner sc = new Scanner(System.in);
+        do {
+            line = sc.nextLine();
+            if (!line.equals("FIM")) {
+                try {
+                    series.inserirFim(Read.readClass(line));
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        } while (!line.equals("FIM"));
+
+        int numOfComands = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < numOfComands; i++) {
+            line = sc.nextLine();
+            switch (line.substring(0, 2)) {
+            case "II":
+                try {
+                    series.inserirInicio(Read.readClass(line.substring(3)));
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                break;
+
+            case "I*":
+                try {
+                    int index = Integer.parseInt(line.substring(3, 5));
+                    Serie aux = Read.readClass(line.substring(6));
+                    series.inserir(aux, index);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                break;
+
+            case "IF":
+                try {
+                    series.inserirFim(Read.readClass(line.substring(3)));
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                break;
+
+                case "RI":
+                try {
+                   Serie aux = series.removerInicio();
+                   System.out.println("(R) " + aux.getName());
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                break;
+
+                case "R*":
+                try {
+                    int index = Integer.parseInt(line.substring(3, 5));
+                    Serie aux = series.remover(index);
+                    System.out.println("(R) " + aux.getName());
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                break;
+
+                case "RF":
+                try {
+                   Serie aux = series.removerFim();
+                   System.out.println("(R) " + aux.getName());
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                break;
+            }
+        }
+
+        series.mostrar();
+
+    }
+}
